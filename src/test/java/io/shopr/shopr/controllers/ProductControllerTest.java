5e37de4e76dd8554575c5ee3d7cecab6c52d0238
Thomas Andersen <thomas.andersen77@gmail.com>
2019-01-26 13:51:13 +0100

    created custom annotation for integration tests

--- src/test/java/io/shopr/shopr/controllers/ProductControllerTest.java
+++ src/test/java/io/shopr/shopr/controllers/ProductControllerTest.java
@@ -1,11 +1,12 @@
 package io.shopr.shopr.controllers;
 
+import io.shopr.shopr.testutils.IntegrationTest;
 import io.shopr.shopr.entities.Category;
 import io.shopr.shopr.entities.Product;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,22 +14,26 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
+
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@IntegrationTest
 public class ProductControllerTest {
 
     @Autowired
     TestRestTemplate template;
+    @Autowired
+    TestEntityManager testEntityManager;
 
     @Test
     public void createProduct() {
         ResponseEntity<Long> response = template.postForEntity("/product", new Product("Epler", 15.00, new Category("Frukt"), 6), Long.class);
         assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertTrue(response.getBody().intValue() > 0L);
+        assertTrue(response.getBody() != null && response.getBody().intValue() > 0L);
     }
 
     @Test
     public void getProductList() {
+        assertNotNull(testEntityManager);
     }
 
     @Test
---
+++ src/test/java/io/shopr/shopr/testutils/IntegrationTest.java
@@ -0,0 +1,20 @@
+package io.shopr.shopr.testutils;
+
+import io.shopr.shopr.ShoprApplication;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = ShoprApplication.class)
+@AutoConfigureDataJpa
+@AutoConfigureTestDatabase
+@AutoConfigureTestEntityManager
+public @interface IntegrationTest {
+}
