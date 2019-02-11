94f2ec2bf47d6694e6a9805e6988ebfa4a07b017
Thomas Andersen <thomas.andersen@knowit.no>
2019-02-11 22:11:18 +0100

    spring-security

--- shopr-repository/src/main/java/io/shopr/repositories/domain/User.java
+++ shopr-repository/src/main/java/io/shopr/repositories/domain/User.java
@@ -1,21 +1,39 @@
 package io.shopr.repositories.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "Users")
 public class User {
     @Id
     @GeneratedValue(strategy =   GenerationType.IDENTITY)
     private Long id;
+    @Column
+    private String username;
+    @Column
+    private String password;
+    @Column
+    private boolean enabled;
 
-    public User() {
+    public User(String username, String password, boolean enabled) {
+        this.username = username;
+        this.password = password;
+        this.enabled = enabled;
     }
 
     public Long getId() {
         return id;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
--- shopr-webservice/src/main/java/io/shopr/configuration/WebConfiguration.java
+++ shopr-webservice/src/main/java/io/shopr/configuration/WebConfiguration.java
@@ -7,9 +7,18 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-public class WebConfiguration {
+@EnableWebSecurity(debug = true)
+public class WebConfiguration extends WebSecurityConfigurerAdapter {
 
     public WebConfiguration() {
     }
@@ -25,5 +34,33 @@ public class WebConfiguration {
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("thomas")
+                        .password("pass")
+                        .roles("ADMIN")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 }
